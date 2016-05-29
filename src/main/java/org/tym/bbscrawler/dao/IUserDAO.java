7b3b30542095d8fb23ab6f03e35fa87c75792fd4
tangtomorrow <tangyaming_nju@sina.com>
2016-05-30 01:31:56 +0800

    add IUserDAO.queryAllUsers()

--- src/main/java/org/tym/bbscrawler/dao/IUserDAO.java
+++ src/main/java/org/tym/bbscrawler/dao/IUserDAO.java
@@ -1,5 +1,7 @@
 package org.tym.bbscrawler.dao;
 
+import java.util.List;
+
 import org.tym.bbscrawler.model.User;
 
 public interface IUserDAO {
@@ -17,4 +19,10 @@ public interface IUserDAO {
 	
 	// 获取当前已保存的用户数
 	public int getUserNum();
+	
+	// 分页查找
+	//public List<User> queryUsersLimit(int startIndex, int count);
+	
+	// 查找所有用户
+	public List<User> queryAllUsers();
 }
