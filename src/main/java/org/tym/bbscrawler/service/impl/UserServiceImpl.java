18d1e5cbd0d0a2aa860dc718260d59203cb8c097
tangtomorrow <tangyaming_nju@sina.com>
2016-06-07 14:18:42 +0800

    remove @override

--- src/main/java/org/tym/bbscrawler/service/impl/UserServiceImpl.java
+++ src/main/java/org/tym/bbscrawler/service/impl/UserServiceImpl.java
@@ -13,32 +13,26 @@ public class UserServiceImpl implements IUserService {
 
 	private IUserDAO userDAO;
 
-	@Override
 	public int insertUser(User user) {
 		return userDAO.insertUser(user);
 	}
 
-	@Override
 	public User findUserById(int id) {
 		return userDAO.findUserById(id);
 	}
 
-	@Override
 	public User findUserByUserid(String userid) {
 		return userDAO.findUserByUserid(userid);
 	}
 
-	@Override
 	public int updateUser(User user) {
 		return userDAO.updateUser(user);
 	}
 
-	@Override
 	public int getUserNum() {
 		return userDAO.getUserNum();
 	}
 
-	@Override
 	public List<User> queryAllUsers() {
 		return userDAO.queryAllUsers();
 	}
