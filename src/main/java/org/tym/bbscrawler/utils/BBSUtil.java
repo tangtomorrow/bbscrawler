3bfe12eff012d57ff4b6ae0a8993931dfd889943
tangtomorrow <tangyaming_nju@sina.com>
2016-06-09 01:32:51 +0800

    增加更新logininfo表的功能

--- src/main/java/org/tym/bbscrawler/utils/BBSUtil.java
+++ src/main/java/org/tym/bbscrawler/utils/BBSUtil.java
@@ -21,6 +21,7 @@ import org.tym.bbscrawler.model.User;
 public class BBSUtil {
 	/**
 	 * 获取版面名列表
+	 * 
 	 * @return
 	 */
 	public static List<String> getAllBoardNames() {
@@ -28,10 +29,9 @@ public class BBSUtil {
 
 		InputStream in = null;
 		try {
-			//in = new FileInputStream("bbsboard/Boards.xml");
+			// in = new FileInputStream("bbsboard/Boards.xml");
 			/*
-			 * 因为maven打包后资源路径会变，因此使用了mybatis的读取资源的工具
-			 * 具体实现原理再看～
+			 * 因为maven打包后资源路径会变，因此使用了mybatis的读取资源的工具 具体实现原理再看～
 			 */
 			in = Resources.getResourceAsStream("bbsboard/Boards.xml");
 			Document doc = Jsoup.parse(in, "UTF-8", "", Parser.xmlParser());
@@ -50,33 +50,32 @@ public class BBSUtil {
 
 		return boardList;
 	}
-	
+
 	/**
-	 * 更新数据库表中的所有纪录
+	 * 更新数据库user表中的所有纪录
 	 */
 	public static void updateAllUsers() {
 		// 已有数据的记录数
 		int totalCount = ServiceUtil.getUserService().getUserNum();
-		totalCount = 10;
-		
+
 		for (int i = 1; i <= totalCount; i++) {
 			long start = new Date().getTime();
-			
+
 			try {
 				Thread.sleep(500);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
-			
+
 			User userExist = ServiceUtil.getUserService().findUserById(i);
-			//System.out.println(userExist);
+			// System.out.println(userExist);
 			if (userExist != null) {
 				String userid = userExist.getUserid();
 				User user = NetUtil.parseUserUrl(BBSUrl.UserPrefix + userid);
 				if (!user.equals(userExist)) {
 					System.out.println("[Update]:\t" + user.getUserid());
 					ServiceUtil.getUserService().updateUser(user);
-					
+
 					LoginInfo loginInfo = new LoginInfo();
 					loginInfo.setUserid(user.getUserid());
 					loginInfo.setLoginip(user.getLastLoginIp());
@@ -85,10 +84,39 @@ public class BBSUtil {
 					}
 				}
 			}
-			
+
 			long end = new Date().getTime();
-			//System.out.println(end - start + "ms");
-			
+			// System.out.println(end - start + "ms");
+
+		}
+	}
+
+	/**
+	 * 向数据库logininfo表中插入纪录
+	 */
+	public static void updateAllUsersLoginInfo() {
+		// 已有数据的记录数
+		int totalCount = ServiceUtil.getUserService().getUserNum();
+
+		for (int i = 1; i <= totalCount; i++) {
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+
+			User userExist = ServiceUtil.getUserService().findUserById(i);
+			// System.out.println(userExist);
+			if (userExist != null) {
+				String userid = userExist.getUserid();
+				User user = NetUtil.parseUserUrl(BBSUrl.UserPrefix + userid);
+				LoginInfo loginInfo = new LoginInfo();
+				loginInfo.setUserid(user.getUserid());
+				loginInfo.setLoginip(user.getLastLoginIp());
+				if (ServiceUtil.getLoginInfoService().findLoginInfoByUseridLoginip(loginInfo) == -1) {
+					ServiceUtil.getLoginInfoService().insertLoginInfo(loginInfo);
+				}
+			}
 		}
 	}
 }
